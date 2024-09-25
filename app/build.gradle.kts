5bcdd20d67662b26b8c78c7afdb6670af4438dab
JaviSerrano <jsermar1@epsg.upv.es>
2024-09-25 13:47:53 +0200

    DIOS TEN PIEDAD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.serrano.proyectoandroid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.serrano.proyectoandroid"
